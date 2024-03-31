fd24def435fd2095f83b199b2f8e25932b12db14
Ankit Maurya <96623227+am-1145@users.noreply.github.com>
2024-03-31 17:45:05 +0530

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import connectDB from "../db/connect.js";
+import User from "../models/user.model.js";
+import userRoutes from "../routes/user.routes.js";
+import teamRoutes from "../routes/team.routes.js";
+import cors from "cors";
+import data from "../userData.json" assert { type: "json" };
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+https: app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/api/users", userRoutes);
+app.use("/api/team", teamRoutes);
+
+const uploadUsers = async () => {
+  try {
+    await User.deleteMany({});
+    const users = await User.create(data);
+    console.log("Users uploaded to DB");
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(uploadUsers);
+  uploadUsers();
+};
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server is running on port 3000");
+});
